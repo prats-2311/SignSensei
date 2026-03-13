c6727fe22fc270cff0057e47d808a1d82eb90646
Prateek Srivastava <prateek.srivastava2311@gmail.com>
2026-03-14 01:11:14 +0530

    creating a .dockerignore

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,11 @@ app = FastAPI(title="SignSensei Token Service")
 # Allow requests from our React frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*","http://localhost:5173", "http://127.0.0.1:5173"], 
+    allow_origins=[
+        "https://signsensei.web.app",
+        "http://localhost:5173", 
+        "http://127.0.0.1:5173"
+    ], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
